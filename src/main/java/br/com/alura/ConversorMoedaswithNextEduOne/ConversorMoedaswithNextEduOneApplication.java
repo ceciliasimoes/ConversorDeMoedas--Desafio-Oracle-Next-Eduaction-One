c5dbbf73ab539b1dd9491e94be5f9e6a238a9b24
AC <acsimoes446@gmail.com>
2024-07-02 16:01:42 -0300

    feat: iniciando projeto

---
+++ src/main/java/br/com/alura/ConversorMoedaswithNextEduOne/ConversorMoedaswithNextEduOneApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.ConversorMoedaswithNextEduOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMoedaswithNextEduOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorMoedaswithNextEduOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/ConversorMoedaswithNextEduOne/ConversorMoedaswithNextEduOneApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.ConversorMoedaswithNextEduOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorMoedaswithNextEduOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

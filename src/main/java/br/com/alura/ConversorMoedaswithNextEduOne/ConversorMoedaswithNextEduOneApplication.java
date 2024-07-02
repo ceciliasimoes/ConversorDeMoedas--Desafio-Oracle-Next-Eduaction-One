23aea7b11ccd39e5c6f52b46c0d1c857dc4eb93e
AC <acsimoes446@gmail.com>
2024-07-02 18:39:06 -0300

    feat(CommandLineRunner): tornando o programa executavel via linha de comando

--- src/main/java/br/com/alura/ConversorMoedaswithNextEduOne/ConversorMoedaswithNextEduOneApplication.java
+++ src/main/java/br/com/alura/ConversorMoedaswithNextEduOne/ConversorMoedaswithNextEduOneApplication.java
@@ -1,13 +1,21 @@
 package br.com.alura.ConversorMoedaswithNextEduOne;
 
+import br.com.alura.ConversorMoedaswithNextEduOne.services.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConversorMoedaswithNextEduOneApplication {
+public class ConversorMoedaswithNextEduOneApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConversorMoedaswithNextEduOneApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.exibeMenu();
+
+	}
 }
